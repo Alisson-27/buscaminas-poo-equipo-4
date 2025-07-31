de34fa43066e948ef46ae860a92f946344acab78
FranciscoLitardo <fco.litardo64@gmail.com>
2025-07-31 01:18:02 -0500

    Main.java

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -1,5 +1,17 @@
 package aplicacion;
 
-public class Main {
+import controlador.JuegoControlador;
+import modelo.Jugador;
+import modelo.Partida;
+import vista.ConsolaVista;
 
+public class Main {
+    public static void main(String[] args) {
+        ConsolaVista vista = new ConsolaVista();
+        Jugador jugador = new Jugador("Jugador 1");
+        Partida partida = new Partida(jugador);
+        JuegoControlador controlador = new JuegoControlador(partida, vista);
+
+        controlador.iniciarJuego();
+    }
 }
