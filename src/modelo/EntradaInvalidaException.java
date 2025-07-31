c0e3e065a568d4ddc3cb2715362f0fb2cf6ece5f
FranciscoLitardo <fco.litardo64@gmail.com>
2025-07-31 01:19:42 -0500

    EntradaInvalidaException.java

--- src/modelo/EntradaInvalidaException.java
+++ src/modelo/EntradaInvalidaException.java
@@ -1,10 +1,7 @@
 package modelo;
 
-public class EntradaInvalidaException {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class EntradaInvalidaException extends Exception {
+    public EntradaInvalidaException(String mensaje) {
+        super(mensaje);
+    }
 }
