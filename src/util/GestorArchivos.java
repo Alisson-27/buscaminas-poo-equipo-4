6f66b0417c03afa763e8f9898ef92eb81df438c9
DESKTOP-1OP67C8\mog_0 <martinortiz0989@gmail.com>
2025-07-31 22:48:18 -0500

    Martin Ortiz primer commit

--- src/util/GestorArchivos.java
+++ src/util/GestorArchivos.java
@@ -1,5 +1,28 @@
+
 package util;
+
+import modelo.Partida;
+
+import java.io.*;
 
 public class GestorArchivos {
 
+    private static final String ARCHIVO_GUARDADO = "guardado.dat";
+
+    public static void guardarPartida(Partida partida) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ARCHIVO_GUARDADO))) {
+            oos.writeObject(partida);
+        }
+    }
+
+    public static Partida cargarPartida() throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ARCHIVO_GUARDADO))) {
+            return (Partida) ois.readObject();
+        }
+    }
+
+    public static boolean existeGuardado() {
+        File archivo = new File(ARCHIVO_GUARDADO);
+        return archivo.exists();
+    }
 }
