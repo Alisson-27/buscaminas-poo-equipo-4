cd35c5e0c24e77869077045e0ddb930c5844ff4a
Alisson <alissonalejandra055@gmail.com>
2025-08-03 21:49:49 -0500

    Actualizo clases Jugador y ConsolaVista con implementación completa

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -1,5 +1,13 @@
 package modelo;
 
 public class Jugador {
+    private String nombre;
 
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
--- src/vista/ConsolaVista.java
+++ src/vista/ConsolaVista.java
@@ -1,5 +1,29 @@
 package vista;
 
+import modelo.Tablero;
+import modelo.Casilla;
+
 public class ConsolaVista {
 
+    public void mostrarTablero(Tablero tablero) {
+        System.out.print("   ");
+        for (int col = 1; col <= Tablero.COLUMNAS; col++) {
+            System.out.print(col + " ");
+        }
+        System.out.println();
+
+        for (int fila = 0; fila < Tablero.FILAS; fila++) {
+            char letra = (char) ('A' + fila);
+            System.out.print(letra + "  ");
+            for (int col = 0; col < Tablero.COLUMNAS; col++) {
+                Casilla casilla = tablero.getCasilla(fila, col);
+                System.out.print(casilla.toString() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
 }
