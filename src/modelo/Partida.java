b444a332a51d237858c446cfcd0957332456db3c
Alisson <alissonalejandra055@gmail.com>
2025-08-03 21:54:19 -0500

    Agrego implementación completa de la clase Partida

--- src/modelo/Partida.java
+++ src/modelo/Partida.java
@@ -1,5 +1,46 @@
 package modelo;
 
 public class Partida {
+    private Tablero tablero;
+    private Jugador jugador;
+    private boolean terminado;
+    private boolean ganado;
 
+    public Partida(Jugador jugador) {
+        this.jugador = jugador;
+        this.tablero = new Tablero();
+        this.terminado = false;
+        this.ganado = false;
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public boolean isTerminado() {
+        return terminado;
+    }
+
+    public boolean isGanado() {
+        return ganado;
+    }
+
+    public void descubrirCasilla(int fila, int columna) throws CasillaYaDescubiertaException {
+        tablero.descubrirCasilla(fila, columna);
+
+        if (tablero.getCasilla(fila, columna).tieneMina()) {
+            terminado = true;
+            ganado = false;
+            return;
+        }
+
+        if (tablero.todasCasillasSegurasDescubiertas()) {
+            terminado = true;
+            ganado = true;
+        }
+    }
 }
